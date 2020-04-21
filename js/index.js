fb703952b488b77147ebadedbe2b22ed5b6c1bca
Ahmedmubarakweb <58855051+Ahmedmubarakweb@users.noreply.github.com>
2020-04-21 13:38:05 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+$('.cards').slick({
+  dots: true,
+  infinite: true,
+  speed: 6000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay:true,
+
+});
+
+$('.carousel').carousel({
+  interval: 6000
+})
